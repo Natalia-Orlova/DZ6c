ccd81efa5172d302887a6fbd2f9560737b2103f7
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-24 19:18:43 +0400

    решение task0

--- task0/Program.cs
+++ task0/Program.cs
@@ -3,3 +3,48 @@
 
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество элементов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m == 0)
+{
+    Console.WriteLine("Недопустимое значение");
+    return;
+}
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i}-й элемент: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[i] = num;
+    }
+    return array;
+}
+int[] result = FillArray(m);
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}");
+}
+
+int GetPosNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+PrintArray(result);
+int res = GetPosNumber(result);
+Console.WriteLine($"Количество положительных чисел: {res}");
